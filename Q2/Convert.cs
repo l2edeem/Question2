8a171c90c121891e78782cee63f94fdf763261a3
Ougrid339 <ougrid3top@gmail.com>
2022-04-07 16:35:40 +0700

    first commit

---
+++ Q2/Convert.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    public class Convert
+    {
+        public double ConvertWON(double thb)
+        {
+            double result = thb*33.72;
+            return result;
+        }
+        public double ConvertUSD(double thb)
+        {
+            double result = thb * 0.028;
+            return result;
+        }
+        public double ConvertYEN(double thb)
+        {
+            double result = thb * 3.4;
+            return result;
+        }
+    }
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double thbConvert = 0;
+            double thb;
+            int chktypeErr = 0;
+            Convert convrt = new Convert();
+            Console.Write("Input :Number on THB ");
+            string strthb = Console.ReadLine();
+            Console.Write("      :String:Convert To Currency Type ");
+            try
+            {
+                var type = Console.ReadLine();
+                thb = Double.Parse(strthb);
+                if (type.ToUpper() == "WON")
+                {
+                    thbConvert = convrt.ConvertWON(thb);
+                }
+                else if (type.ToUpper() == "USD")
+                {
+                    thbConvert = convrt.ConvertUSD(thb);
+                }
+                else if (type.ToUpper() == "YEN")
+                {
+                    thbConvert = convrt.ConvertYEN(thb);
+                }
+                else
+                {
+                    chktypeErr = 1;
+                }
+
+                if (chktypeErr == 1)
+                {
+                    Console.WriteLine("Output:String:System is not Convert Type " + type);
+                }
+                else
+                {
+                    Console.WriteLine("Output:String:Number of converted " + thbConvert + " " + type);
+                }
+                Console.Read();
+            }
+            catch (Exception ex){
+                Console.WriteLine(ex);
+                Console.Read();
+            }
+            
+        }
+    }
+}
